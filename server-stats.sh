0ea0f8bb5f1423e5f75ab82050348230bef712d9
Lefter Sotiri <leftersotiri@proton.me>
2025-09-09 03:58:25 +0300

    Initiliaze commit

---
+++ server-stats.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+get_cpu_usage(){
+echo "CPU Usage:"
+mpstat | awk '$12~/[0-9.]+/{print100-$12"%used"}'
+}
+
+get_memory_usage(){
+echo "Memmory Usage:"
+free -m | awk 'NR==2{printf"Memory Usage: %s/%sMB (%.2f%%)\n",
+$3,$2,$3*100/$2}'
+}
+
+get_disk_usage()
+{
+echo "Disk Usage:"
+df -h -total | awk 'END{print"Used: "$3", Free: "$4", Usage: "$5}'
+}
+
+get_extra_stats()
+{
+echo "OS Version:"
+lsb_release -a 2>/dev/null || cat /etc/os-release
+
+echo "Uptime:"
+uptime
+
+echo "Load Average:"
+cat /proc/loadavg
+
+echo "Logged in users:"
+who
+}
+
+main(){
+echo "Server Perfomance Stats"
+echo "------------------------"
+get_cpu_usage
+get_memory_usage
+get_disk_usage
+get_extra_stats
+}
+main
